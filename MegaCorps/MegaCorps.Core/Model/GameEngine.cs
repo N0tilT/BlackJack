4b6de1cbee8b679bba42372bd8820bbf8d2facbb
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2024-02-06 22:40:57 +0300

    Player model

--- MegaCorps/MegaCorps.Core/Model/GameEngine.cs
+++ MegaCorps/MegaCorps.Core/Model/GameEngine.cs
@@ -2,6 +2,7 @@
 using MegaCorps.Core.Model.Enums;
 using MegaCorps.Core.Model.GameUtils;
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,93 +14,48 @@ namespace MegaCorps.Core.Model
     {
         private Deck _deck;
         public Deck Deck { get => _deck; set => _deck = value; }
+        public List<Player> Players { get => _players; set => _players = value; }
 
-        private List<GameCard> _firstPlayerHand;
-        private List<GameCard> _secondPlayerHand;
-        private List<GameCard> _thirdPlayerHand;
-        private List<GameCard> _fourthPlayerHand;
-        public List<GameCard> FirstPlayerHand { get => _firstPlayerHand; set => _firstPlayerHand = value; }
-        public List<GameCard> SecondPlayerHand { get => _secondPlayerHand; set => _secondPlayerHand = value; }
-        public List<GameCard> ThirdPlayerHand { get => _thirdPlayerHand; set => _thirdPlayerHand = value; }
-        public List<GameCard> FourthPlayerHand { get => _fourthPlayerHand; set => _fourthPlayerHand = value; }
-        public int FirstPlayerScore { get; set; }
-        public int SecondPlayerScore { get; set; }
-        public int ThirdPlayerScore { get; set; }
-        public int FourthPlayerScore { get; set; }
+        private List<Player> _players;
 
         public GameEngine()
         {
             Deck = DeckBuilder.GetDeck();
             Deck.Shuffle();
-            FirstPlayerScore = 0;
-            SecondPlayerScore = 0;
-            ThirdPlayerScore = 0;
-            FourthPlayerScore = 0;
+            Players = new List<Player> { new Player(), new Player(), new Player(), new Player() };
         }
 
         public void Deal(int dealCount)
         {
             List<List<GameCard>> hands = Deck.Deal(dealCount, 4);
-            FirstPlayerHand = hands[0];
-            SecondPlayerHand = hands[1];
-            ThirdPlayerHand = hands[2];
-            FourthPlayerHand = hands[3];
+
+            for (int i = 0; i < Players.Count; i++)
+            {
+                Player player = Players[i];
+                player.Hand = new PlayerHand(hands[i]);
+            }
         }
 
         public void Turn()
         {
-            //В деке пихать из анплеед в плеед сыгранные карты
+
+
         }
 
         public void SelectCard(GameCard card, int playerPosition)
         {
             int cardIndex;
-            switch (playerPosition)
+            for (int i = 0; i < Players.Count; i++)
             {
-                case 0:
-                    cardIndex = FirstPlayerHand.FindIndex((element) => element.Id == card.Id);
-                    if (FirstPlayerHand[cardIndex].State == CardState.Used)
-                    {
-                        FirstPlayerHand[cardIndex].State = CardState.Unused;
-                    }
-                    else if (FirstPlayerHand[cardIndex].State == CardState.Unused)
-                    {
-                        FirstPlayerHand[cardIndex].State = CardState.Used;
-                    }
-                    break;
-                case 1:
-                    cardIndex = SecondPlayerHand.FindIndex((element) => element.Id == card.Id);
-                    if (SecondPlayerHand[cardIndex].State == CardState.Used)
-                    {
-                        SecondPlayerHand[cardIndex].State = CardState.Unused;
-                    }
-                    else if (SecondPlayerHand[cardIndex].State == CardState.Unused)
-                    {
-                        SecondPlayerHand[cardIndex].State = CardState.Used;
-                    }
-                    break;
-                case 2:
-                    cardIndex = ThirdPlayerHand.FindIndex((element) => element.Id == card.Id);
-                    if (ThirdPlayerHand[cardIndex].State == CardState.Used)
-                    {
-                        ThirdPlayerHand[cardIndex].State = CardState.Unused;
-                    }
-                    else if (ThirdPlayerHand[cardIndex].State == CardState.Unused)
-                    {
-                        ThirdPlayerHand[cardIndex].State = CardState.Used;
-                    }
-                    break;
-                case 3:
-                    cardIndex = FourthPlayerHand.FindIndex((element) => element.Id == card.Id);
-                    if (FourthPlayerHand[cardIndex].State == CardState.Used)
-                    {
-                        FourthPlayerHand[cardIndex].State = CardState.Unused;
-                    }
-                    else if (FourthPlayerHand[cardIndex].State == CardState.Unused)
-                    {
-                        FourthPlayerHand[cardIndex].State = CardState.Used;
-                    }
-                    break;
+                cardIndex = Players[i].Hand.Cards.FindIndex((element) => element.Id == card.Id);
+                if (Players[i].Hand.Cards[cardIndex].State == CardState.Used)
+                {
+                    Players[i].Hand.Cards[cardIndex].State = CardState.Unused;
+                }
+                else if (Players[i].Hand.Cards[cardIndex].State == CardState.Unused)
+                {
+                    Players[i].Hand.Cards[cardIndex].State = CardState.Used;
+                }
             }
         }
     }
---
+++ MegaCorps/MegaCorps.Core/Model/Player.cs
@@ -0,0 +1,41 @@
+﻿using MegaCorps.Core.Model.Cards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaCorps.Core.Model
+{
+    public class Player
+    {
+        private PlayerHand _hand;
+        private List<GameCard> _targeted;
+        private int _score;
+        public Player() { Score = 1; }
+
+        public PlayerHand Hand { get => _hand; set => _hand = value; }
+        public List<GameCard> Targeted { get => _targeted; set => _targeted = value; }
+        public int Score { get => _score; set => _score = value; }
+
+        public void PlayHand()
+        {
+            Score += Hand.Cards.Where((card) => card.Color == "Green").Count() - _targeted.Count();
+            Score += Hand.Cards.Where((card) => card.Color == "Yellow").Count();
+        }
+    }
+
+    public class PlayerHand
+    {
+        private List<GameCard> _cards;
+
+        public PlayerHand(List<GameCard> cards)
+        {
+            this._cards = cards;
+        }
+
+        public PlayerHand() { }
+
+        public List<GameCard> Cards { get => _cards; set => _cards = value; }
+    }
+}
